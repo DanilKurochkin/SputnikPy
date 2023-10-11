8c67028bc64e95cf8f344ceae2a792bdf7f36926
DanilKurochkin <78516434+DanilKurochkin@users.noreply.github.com>
2023-10-11 23:58:13 +0300

    before rollback

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from classes.models import *
 from classes.orbits import SunLookingOrbit
 
 def main():
-    A6061 = Material(9000, 2700, 209) #алюминий 6061
+    A6061 = Material(900, 2700, 209) #алюминий 6061
     coat = Coating(0.5, 0.6)
     orbit = SunLookingOrbit(500)
     cond = Conditions()
@@ -14,12 +14,11 @@ def main():
     sp = Sputnik(1, 2, 3, 0.001, A6061, coat, orbit)
     sp.createVolumes(20)
     sp.knitPlates()
-    
+    wl.Connect.neighbours(sp, 0.1)
     cond.addEx(wl.Sun(1500), wl.Radiaton())
     sp.addCondition(cond)
-    wl.Connect.neighbours(sp, 0.1)
     start = time.time()
-    sp.solve(50, 1000, 1, 250, radiation_check=True)
+    sp.solve(20, 1000, 1, 250, radiation_check=True)
     end = time.time()
     
     print(end-start)

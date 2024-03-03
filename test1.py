d5ebfa3433f2835cb9efe55554659e09e6613861
DanilKurochkin <78516434+DanilKurochkin@users.noreply.github.com>
2024-03-04 00:36:04 +0300

    With R

--- test1.py
+++ test1.py
@@ -5,29 +5,31 @@ from classes.material import Material, Coating
 from classes.results import Checker
 import time
         
-mat = Material(c=8000, p=2700, k=100) #материал спутника
-coat = Coating(As=0.9, epsilon=0.5) #покрытие
+mat = Material(c=800, p=2700, k=100) #материал спутника
+coat = Coating(As=0.6, epsilon=0.4) #покрытие
 orbit = SunLookingOrbit(radiusAboveEarth=500) #орбита
 
 #создание спутника
-sp = Sputnik(Lx=1,Ly=2, Lz=3, width=0.001, material=mat, coat=coat, orbit=orbit)  
+sp = Sputnik(Lx=1,Ly=1, Lz=1, width=0.005, material=mat, coat=coat, orbit=orbit)  
 #задание КО в пластинах
 sp.createVolumes(10)
 
 #Создание и подключение граничных условий
 cond = Conditions()
-cond.addEx(wl.Sun(1500), wl.Radiaton())
+cond.addEx(wl.Sun(1367), wl.Radiaton())
 sp.addCondition(cond)
 
 #связывание пластинок
-wl.Connect.neighbours(sputnik=sp, R=0.1)
+wl.Connect.neighbours(sputnik=sp, R=0.01)
 #моделирование
 
 start = time.time()
-sp.solve(60, 1000, 1, 300, radiation_check=True)
+sp.solve(30, 100, 1, 300, radiation_check=True)
 end = time.time()
 print(end-start)
 
 print(Checker.radiationCheck() * 100)
 print(Checker.averageTCheck(sp.size, 100000))
 print(f'Теоретическое:290.89')
+
+import graph
